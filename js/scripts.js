96783f4296adbbce013dd07783c52a6775b8724f
Lena Ha and Nathan Barrett <Guest@Epicodus-9A.local>
2017-10-17 14:44:44 -0700

    make a website displaying definitions to terms

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#jsTitle").click(function() {
+    $("#jsDef").toggleClass("hidden");
+  });
+  $("#opTitle").click(function() {
+    $("#opDef").toggleClass("hidden");
+  });
+  $("#varNamCon").click(function() {
+    $("#varNamingCon").toggleClass("hidden");
+  });
+});
