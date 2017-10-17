109fcadc01e75b440b25de7dfbd5234795cbf3cd
Lena Ha and Nathan Barrett <lena@>
2017-10-17 16:50:02 -0700

    fix everything

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
   $("#jsTitle").click(function() {
-    $("#jsDef").toggleClass("hidden");
+    // $(".initialhidden").children().hide();
+    $("#opDef").addClass("hidden");
+    $("#varNamingCon").addClass("hidden");
+    $("#varNamingCon").toggleClass("hidden");
   });
   $("#opTitle").click(function() {
+    // $(".initialhidden").find('*').hide();
+    $("#varNamingCon").addClass("hidden");
+    $("#jsDef").addClass("hidden");
     $("#opDef").toggleClass("hidden");
   });
   $("#varNamCon").click(function() {
+    // $(".initialhidden").find('*').hide();
+    $("#opDef").addClass("hidden");
+    $("#jsDef").addClass("hidden");
     $("#varNamingCon").toggleClass("hidden");
   });
 });
